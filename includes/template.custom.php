5cc5801a7f7b8cadf16a425895ea04825bb4241c
diafan <garik@diafan.ru>
2017-03-20 20:00:18 +0500

    Update template.custom.php

--- includes/template.custom.php
+++ includes/template.custom.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    DIAFAN.CMS
  *
@@ -7,112 +8,107 @@
  * @license    http://www.diafan.ru/license.html
  * @copyright  Copyright (c) 2003-2016 OOO «Диафан» (http://www.diafan.ru/)
  */
-
-if (! defined('DIAFAN'))
-{
-	$path = __FILE__; $i = 0;
-	while(! file_exists($path.'/includes/404.php'))
-	{
-		if($i == 10) exit; $i++;
-		$path = dirname($path);
-	}
-	include $path.'/includes/404.php';
+if (!defined('DIAFAN')) {
+    $path = __FILE__;
+    $i = 0;
+    while (!file_exists($path . '/includes/404.php')) {
+        if ($i == 10)
+            exit;
+        $i++;
+        $path = dirname($path);
+    }
+    include $path . '/includes/404.php';
 }
 
-class Template extends Diafan
-{
-  new private $twig;
-  new const FROM_VIEW = 'view';
-  new const FROM_TEMPLATE = 'twig';
+class Template extends Diafan {
 
-  new function __construct(&$diafan) {
-    parent::__construct($diafan);
+    new private $twig;
 
-    Custom::inc('plugins/loader.php');
+    new const FROM_VIEW = 'view';
+    new const FROM_TEMPLATE = 'twig';
 
-    $classLoader = new SplClassLoader('Twig', ABSOLUTE_PATH.Custom::path('plugins'));
-    $classLoader->register();
+    new function __construct(&$diafan) {
+        parent::__construct($diafan);
 
-    $loader = new Twig_Loader_Filesystem(ABSOLUTE_PATH);
-    $this->twig = new Twig_Environment($loader, array(
-			'debug' => MOD_DEVELOPER,
-			'cache' => ABSOLUTE_PATH.'cache'
-		));
+        Custom::inc('plugins/loader.php');
 
-    //$classLoader->unregister();
-  }
+        $classLoader = new SplClassLoader('Twig', ABSOLUTE_PATH . Custom::path('plugins'));
+        $classLoader->register();
 
-	new public function getTwig() {
-		return $this->twig;
-	}
+        $loader = new Twig_Loader_Filesystem(ABSOLUTE_PATH);
+        $this->twig = new Twig_Environment($loader, array(
+            'debug' => MOD_DEVELOPER,
+            'cache' => ABSOLUTE_PATH . 'cache'
+        ));
 
-  new protected function findTemplate($from, &$name, $module, $template = '') {
-    $file = null;
-
-    if(defined('IS_MOBILE') && IS_MOBILE)
-		{
-			if($template && Custom::exists('modules/'.$module.'/'.$from.'s/m/'.$module.'.'.$from.'.'.$name.'_'.$template.'.php'))
-			{
-				$file = 'modules/'.$module.'/'.$from.'s/m/'.$module.'.'.$from.'.'.$name.'_'.$template.'.php';
-				$name .= '_'.$template;
-			}
-			if(! $file && Custom::exists('modules/'.$module.'/'.$from.'s/m/'.$module.'.'.$from.'.'.$name.'.php'))
-			{
-				$file = 'modules/'.$module.'/'.$from.'s/m/'.$module.'.'.$from.'.'.$name.'.php';
-			}
-		}
-		if(! $file && $template && Custom::exists('modules/'.$module.'/'.$from.'s/'.$module.'.'.$from.'.'.$name.'_'.$template.'.php'))
-		{
-			$file = 'modules/'.$module.'/'.$from.'s/'.$module.'.'.$from.'.'.$name.'_'.$template.'.php';
-			$name .= '_'.$template;
-		}
-		if(! $file && Custom::exists('modules/'.$module.'/'.$from.'s/'.$module.'.'.$from.'.'.$name.'.php'))
-		{
-			$file = 'modules/'.$module.'/'.$from.'s/'.$module.'.'.$from.'.'.$name.'.php';
-		}
-
-    return $file;
-  }
-
-  new private function normalizeName($name) {
-    return preg_replace('/[^a-z0-9_]+/', '', $name);
-  }
-
-  replace function get($name, $module, $result, $template = '') {
-    $name = $this->normalizeName($name);
-    $current_module = $this->diafan->current_module;
-
-
-    $file = $this->findTemplate(self::FROM_TEMPLATE, $name, $module, $template);
-    if($file) {
-        $this->diafan->current_module = $module;
-        $this->js($name, $module);
-
-        $template = $this->twig->load(Custom::path($file));
-        $text = $template->render($result);
-
-        $this->diafan->current_module = $current_module;
-
-        return $text;
+        //$classLoader->unregister();
     }
 
-    $file = $this->findTemplate(self::FROM_VIEW, $name, $module, $template);
-    if($file) {
-
-			$this->diafan->current_module = $module;
-			$this->js($name, $module);
-
-      ob_start();
-			include(ABSOLUTE_PATH.Custom::path($file));
-			$text = ob_get_contents();
-			ob_end_clean();
-
-      $this->diafan->current_module = $current_module;
-
-      return $text;
+    new public function getTwig() {
+        return $this->twig;
     }
 
-    return '';
-  }
+    new protected function findTemplate($from, &$name, $module, $template = '') {
+        $file = null;
 
+        if (defined('IS_MOBILE') && IS_MOBILE) {
+            if ($template && Custom::exists('modules/' . $module . '/' . $from . 's/m/' . $module . '.' . $from . '.' . $name . '_' . $template . '.php')) {
+                $file = 'modules/' . $module . '/' . $from . 's/m/' . $module . '.' . $from . '.' . $name . '_' . $template . '.php';
+                $name .= '_' . $template;
+            }
+            if (!$file && Custom::exists('modules/' . $module . '/' . $from . 's/m/' . $module . '.' . $from . '.' . $name . '.php')) {
+                $file = 'modules/' . $module . '/' . $from . 's/m/' . $module . '.' . $from . '.' . $name . '.php';
+            }
+        }
+        if (!$file && $template && Custom::exists('modules/' . $module . '/' . $from . 's/' . $module . '.' . $from . '.' . $name . '_' . $template . '.php')) {
+            $file = 'modules/' . $module . '/' . $from . 's/' . $module . '.' . $from . '.' . $name . '_' . $template . '.php';
+            $name .= '_' . $template;
+        }
+        if (!$file && Custom::exists('modules/' . $module . '/' . $from . 's/' . $module . '.' . $from . '.' . $name . '.php')) {
+            $file = 'modules/' . $module . '/' . $from . 's/' . $module . '.' . $from . '.' . $name . '.php';
+        }
+
+        return $file;
+    }
+
+    new private function normalizeName($name) {
+        return preg_replace('/[^a-z0-9_]+/', '', $name);
+    }
+
+    replace function get($name, $module, $result, $template = '') {
+        $name = $this->normalizeName($name);
+        $current_module = $this->diafan->current_module;
+
+
+        $file = $this->findTemplate(self::FROM_TEMPLATE, $name, $module, $template);
+        if ($file) {
+            $this->diafan->current_module = $module;
+            $this->js($name, $module);
+
+            $template = $this->twig->load(Custom::path($file));
+            $text = $template->render($result);
+
+            $this->diafan->current_module = $current_module;
+
+            return $text;
+        }
+
+        $file = $this->findTemplate(self::FROM_VIEW, $name, $module, $template);
+        if ($file) {
+
+            $this->diafan->current_module = $module;
+            $this->js($name, $module);
+
+            ob_start();
+            include(ABSOLUTE_PATH . Custom::path($file));
+            $text = ob_get_contents();
+            ob_end_clean();
+
+            $this->diafan->current_module = $current_module;
+
+            return $text;
+        }
+
+        return '';
+    }
 }
